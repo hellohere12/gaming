851a090a3ba4536990c3d92b7c4b772629f87c7f
hellohere12 <66238848+hellohere12@users.noreply.github.com>
2021-07-25 20:49:56 +0100

    Create games.lua

---
+++ games.lua
@@ -0,0 +1,14 @@
+   local CoreGui = game:GetService("StarterGui")
+        
+        CoreGui:SetCore("SendNotification", {
+         Title = "ProHub";
+         Text = "Check console (F9) to see the supported games.";
+         Duration = 6;
+        }) 
+print("Arsenal")
+        print("Da Hood")
+        print("Saber Simulator")
+        print("Ninja Legends")
+        print("Pressure Wash Simulator")
+        print("Prison Life")
+print("More coming soon!)
